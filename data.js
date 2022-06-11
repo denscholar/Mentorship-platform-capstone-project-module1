f07c28b9ea8b6787ca44355fbf38b8cc55360764
Dennis Akagha <d.akagha20@gmail.com>
2022-06-11 11:57:10 +0100

    new addon

---
+++ data.js
@@ -0,0 +1,211 @@
+/* eslint-disable no-plusplus */
+const data = [
+  {
+    name: 'Egbiri Ifegwu Egbiri',
+    profileImage: './images/speakers/keynote-speaker.jpeg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Keynote Speaker - Legal Counsel: International Monetary Fund (IMF)',
+    longDescr:
+        'Egbiri Egbiri is a Counsel with the Legal Department of the International Monetary Fund, headquartered in Washington DC, United States. Egbiri was ranked as one of the 100 Most Influential Young Nigerians in 2019 (an honoree in the Law and Governance category). He was an awardee of the ESQ Nigerian Legal Awards (2019) in the 40 Under 40 (Nigerian Rising Stars) Category.',
+    link: 'https://www.linkedin.com/in/egbiri-egbiri/',
+  },
+  {
+    name: 'Amb Emmanuel Gabari',
+    profileImage: './images/speakers/DSC-9263.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Moderator',
+    longDescr:
+        'Gabari is a multi-award-winning TV and Radio Broadcaster that presents weekly shows on Shagalinku TV and Vision FM Kano. He is also the Executive Director of African Focus for Youth Development (AFFYD), an NGO that caters to the needs of youth, women and children.',
+    link: 'https://www.linkedin.com/in/amb-emmanuel-gabari-8b1067187/',
+  },
+  {
+    name: 'Mark Asonye',
+    profileImage: './images/speakers/mark.png',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Moderator',
+    longDescr:
+        'Talent development expert, life coach, on air personality at REAL 99.1fm, Aba, writer, political commentator, celebrated counselor, lover of God&#39;s word and founder of an online media outfit - BIGDEAL TALKS.',
+    link: '',
+  },
+  {
+    name: 'Rt. Hon. Sir. Ude Oko Chukwu PhD:',
+    profileImage: './images/speakers/deputy.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Distinguished Special Guest of Honor',
+    longDescr:
+        'The Deputy Governor, Abia State, Nigeria ',
+    link: '',
+  },
+
+  {
+    name: ' Lady Vivien Oko Chukwu',
+    profileImage: './images/speakers/deputy-wife.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Wife of the Deputy Governor, Abia State.',
+    longDescr:
+        'Deputy Governor\'s, Abia State, Nigeria',
+    link: '',
+  },
+  {
+    name: 'Esther Agbon',
+    profileImage: './images/speakers/esther.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Special Guest/Speaker',
+    longDescr: 'Esther Agbon is a public health policy consultant with over 20 years’ experience supporting health policy, advocacy and planning program in Nigeria. Esther is a passionate advocate for Women and girls’ rights.',
+    link: 'https://www.linkedin.com/in/esther-agbon-4a6a7141/',
+  },
+  {
+    name: 'Hon. Dr Kalu Ndukwe',
+    profileImage: './images/speakers/kalu.jpeg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Special Guest/Speaker',
+    longDescr: 'Former Commissioner of Health, Abia State Ministry of Health.',
+    link: '',
+  },
+  {
+    name: 'Chinasa Imo',
+    profileImage: './images/speakers/chinasa.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Founder and Convener',
+    longDescr:
+        'Founder of SieDi-hub, Chinasa is an international development/policy strategist with over nine years’ experience working in the health development sector, she is strongly suited to managing not-for-profit programs that focuses on maternal, new-born, and adolescent reproductive health &amp; rights, systems strengthening, and building participatory processes.',
+    link: 'https://www.linkedin.com/in/chinasa-ude-imo/',
+  },
+  {
+    name: 'Ifegwu Ifeanyi Richard',
+    profileImage: './images/speakers/sino.webp',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Richkid Siino - Performing Art',
+    longDescr:
+        'Siino is an international afro-pop Star, Singer, Songwriter, performing art, from Nkporo in Ohafia Local Government Abia State, Nigeria.',
+    link: '',
+  },
+  {
+    name: 'Precious David Youngman',
+    profileImage: './images/speakers/Precious David Youngman.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Panelist',
+    longDescr:
+        'Precious is a dietitian by profession from the school of Natural Health Science, United Kingdom, she also holds a higher Diploma in diabetes risk awareness, holds a prestigious UK level 7 extended Diploma in Strategic Management.',
+    link: '',
+  },
+  {
+    name: 'Chima Raymond Ukwa',
+    profileImage: './images/speakers/raymond.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Special Guest/Panelist',
+    longDescr:
+        'Chima Raymond Ukwa is a leading Sustainable Development Advocate working on eliminating harmful traditional practices and violence against women and girls.',
+    link: 'https://www.linkedin.com/in/raymondukwa/',
+  },
+  {
+    name: 'Barrister Sam Hart',
+    profileImage: './images/speakers/sam.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Special Guest/Panelist',
+    longDescr:
+        'Sam Hart Is a lawyer, Communicator, an ACUMEN Fellow, and the Chancellor at Sam Hart Center for Strategic Leadership. He is the Current Director General of Abia State Marketing and  Quality Management Agency, and the Special Advisor to the Abia State Governor on Strategic Communications.',
+    link: '',
+  },
+  {
+    name: 'Ifeoma Anisha Okenwa',
+    profileImage: './images/speakers/ifeoma.jpeg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Brand Ambassador - Youth Inclusivity Program',
+    longDescr:
+        'Ifeoma Anisha Okenwa is a model, a health educator, a youth activist, and a health advocate who specializes in adolescents and Youth health. ',
+    link: 'https://www.linkedin.com/in/anisha-ifeoma-okenwa-a6365223a/',
+  },
+  {
+    name: 'Harshita Kaul',
+    profileImage: './images/speakers/kaul.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Leading community Health Improvement Research',
+    longDescr:
+        'Harshita Kaul is a Doctoral Researcher, pursuing her PhD in Fundamental Biology of Ageing and Ageing Related Disorders, pecifically focusing on mitochondrial dysfunction and diabetes, obesity and lifestyle interventions in Cologne Graduate School of Ageing / EIT Health Ageing PhD School, Germany.',
+    link: 'https://www.linkedin.com/in/harshita-kaul-95340255/',
+  },
+
+  {
+    name: 'Rejoice Amaka Ukah',
+    profileImage: './images/speakers/rejoice.jpg',
+    bgImage: './images/draught.PNG',
+    shortDesc: 'Brand ambassador Sisters’ Connect.',
+    longDescr:
+        'Rejoice Amaka is a gender activist, a girl child education advocate, a visionary and media personality, and the Executive Director of ChuksIcon&#39;s World initiative and Icon’s Concept',
+    link: 'https://www.linkedin.com/in/rejoice-amaka-7415b1239/',
+  },
+
+];
+
+// create a template
+const partner = document.querySelector('.partner');
+const body = document.querySelectorAll('body');
+const container = document.createElement('div');
+body[0].insertBefore(container, partner);
+container.className = 'featured-mentor-section';
+
+const title = document.createElement('h2');
+title.className = 'mentor-title';
+title.textContent = 'Featured Speakers and Guest';
+container.appendChild(title);
+
+const line = document.createElement('p');
+line.className = 'line';
+container.appendChild(line);
+
+const grid = document.createElement('div');
+grid.className = 'mentors-grid';
+container.appendChild(grid);
+
+const button = document.createElement('button');
+button.className = 'see-more';
+button.type = 'button';
+button.textContent = 'See More Guest';
+container.appendChild(button);
+
+const icon = document.createElement('i');
+icon.className = 'fas fa-chevron-down';
+button.appendChild(icon);
+
+const mentorsData = () => {
+  for (let i = 0; i < data.length; i++) {
+    const gridItems = `
+        <div class="grid-items-speakers">
+            <div class="img-wrapper">
+              <img class="img-draught" src="${data[i].bgImage}" alt="background image" />
+              <img class="img-speaker" src="${data[i].profileImage}"alt="mentor image" />
+            </div>
+            <div class="mentor-profile">
+              <h3>${data[i].name}</h3>
+              <p class="profile">${data[i].shortDesc}</p>
+              <p class="line"></p>
+              <p class="works">${data[i].longDescr}</p>
+              <h4><a class="profile-link" href="${data[i].link}" target="_blank" rel="noopener noreferrer">${data[i].link === '' ? '' : 'Visit profile'}</a></h4>
+            </div>
+        </div>
+        `;
+    const newContainer = document.createElement('div');
+    newContainer.className = 'mentors-grid-items';
+    newContainer.innerHTML = gridItems;
+    grid.appendChild(newContainer);
+  }
+};
+
+mentorsData();
+
+// see more functionality
+const seeMore = document.querySelector('.see-more');
+let currentItem = 4;
+
+seeMore.addEventListener('click', () => {
+  const boxes = [...document.querySelectorAll('.featured-mentor-section .mentors-grid .mentors-grid-items')];
+  for (let i = currentItem; i < currentItem + 4; i++) {
+    boxes[i].style.display = 'inline-block';
+  }
+  currentItem += 3;
+
+  if (currentItem >= boxes.length - 2) {
+    seeMore.style.display = 'none';
+  }
+});
